cd8b056a3dec43a94f10f0e097965493a27ce858
黃翌深 <87598684+Le-Shunny@users.noreply.github.com>
2024-03-25 15:18:07 +0000

    update osu.js

--- scripts/cmds/osu.js
+++ scripts/cmds/osu.js
@@ -1,34 +1,40 @@
-const axios = require('axios');
+const axios = require("axios");
 module.exports = {
   config: {
     name: "osu",
-    aliases: ["screenshot"],
+    aliases: ["OSU!"],
     version: "1.0",
-    author: "MILAN",
+    author: "Shen",
     countDown: 5,
     role: 0,
-    shortDescription: "get screenshot of website",
-    longDescription: "get screenshot of website",
+    shortDescription: "get osu stats card",
+    longDescription: "get osu stats card",
     category: "media",
-    guide: "{pn} link"
+    guide: "{pn} <username> <mode> <language>",
   },
 
   onStart: async function ({ message, args }) {
-    const mode = args.join(" ");
-    if (!mode) {
-      return message.reply(`⚠️ | Please enter a mode!`);
+    const username = args[0];
+    const mode = args[1];
+    const language = args[2];
+    if (!username) {
+      return message.reply("⚠️ | Please enter a username!");
+    } else if (!["std", "taiko", "catch", "mania"].includes(mode)) {
+      return message.reply("⚠️ | Please enter a valid mode (std, taiko, catch, mania)!");
+    } else if (!["en", "cn"].includes(language)) {
+      return message.reply("⚠️ | Please enter a valid language (cn, en)!");
     } else {
       try {
-        const BASE_URL = `https://image.thum.io/get/width/1920/crop/400/fullpage/noanimate/https://osu-sig.vercel.app/card?user=${args[0]}&mode=${args[1]}&lang=en&w=2002&h=1165`;
+        const BASE_URL = `https://image.thum.io/get/width/1920/crop/400/fullpage/noanimate/https://osu-sig.vercel.app/card?user=${username}&mode=${mode}&lang=${language}&w=2002&h=1165&animation=false`;
         const form = {
-          body: ``
+          body: "",
         };
-        form.attachment = []
+        form.attachment = [];
         form.attachment[0] = await global.utils.getStreamFromURL(BASE_URL);
-        message.reply(form); 
-      } catch (e) { 
-        message.reply(`Error`);
+        message.reply(form);
+      } catch (e) {
+        message.reply("Error");
       }
     }
-  }
+  },
 };
